ee7299784cd7b91391f018cad2db17f99883534d
test 01 - Tuan Anh <dangtuananh94.it@gmail.com>
2026-06-01 01:21:51 +0700

    feat: add solution for challenge 03

---
+++ tests/lesson-04/cloud-kingdom.js
@@ -0,0 +1,9 @@
+let playerName = "Mario";
+let currentLives = 3;
+
+const levelOne = 25;
+const levelTwo = 30;
+const levelThree = 45;
+const totalCoinLevel = levelOne + levelTwo + levelThree;
+
+console.log(totalCoinLevel % 3);
